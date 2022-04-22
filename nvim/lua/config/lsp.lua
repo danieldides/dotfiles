99466cd7b8a4a57b78574d95bd899ab2d4e5d8f1
Daniel Dides <danieldides@gmail.com>
2022-04-22 18:18:17 -0500

    Sync with mac

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -10,10 +10,10 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true
-        },
+        -- ['<CR>'] = cmp.mapping.confirm {
+        --     behavior = cmp.ConfirmBehavior.Replace,
+        --     select = true
+        -- },
         ['<Tab>'] = function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
@@ -92,7 +92,7 @@ local on_attach = function(client, bufnr)
 
 end
 
-local servers = {"pyright", "gopls", "tsserver", "clangd"}
+local servers = {"pyright", "gopls", "tsserver", "clangd", "terraform-ls"}
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
