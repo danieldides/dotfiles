84d68422f96b84c0b8924095f5197f1ffc3a763c
Daniel Dides <danieldides@gmail.com>
2022-07-25 23:25:36 -0500

    nvim: formatter init

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,6 +1,6 @@
 require("config.bling")
 require("config.formatter")
--- require("config.formatter")
+require("config.formatter")
 require("config.lsp")
 require("config.telescope")
 require("config.treesitter")
