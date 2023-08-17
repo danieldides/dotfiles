f1e5f8199c84dcdef97e225c0b14eb388c6aca1f
Daniel Dides <danieldides@gmail.com>
2023-08-16 22:47:30 -0500

    Add pluginst

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -2,7 +2,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'terraform' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -63,5 +63,3 @@ require('nvim-treesitter.configs').setup {
     },
   },
 }
-
-
