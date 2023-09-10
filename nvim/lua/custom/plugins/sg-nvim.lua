33786101997e1e82564606c5d3794a3d9592ae46
Daniel Dides <daniel@Daniels-MBP.T-mobile.com>
2023-09-10 17:42:31 -0500

    nvim: remove sg, guess i don't need it anymore 😔

--- nvim/lua/custom/plugins/sg-nvim.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/sourcegraph/sg.nvim/pull/30/files
-
-return {
-  {
-    'sourcegraph/sg.nvim',
-    branch = 'cody-agent',
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-}
